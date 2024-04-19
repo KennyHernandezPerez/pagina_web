9821e3374b86c1ab4ea26aec80daa231d190822c
KennyHernandezPerez <200300661@ucaribe.edu.mx>
2024-04-19 11:51:35 -0500

    archivos

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { GoogleOAuthProvider } from '@react-oauth/google'
-import App from './App.jsx'
-import './index.css'
-
+const React = require('react');
+const ReactDOM = require('react-dom');
+const { GoogleOAuthProvider } = require('@react-oauth/google');
+const App = require('./App.js');
+require('./index.css');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId="20339060285-jbne00ieud7j9gv5t1u2bl3msucb82cr.apps.googleusercontent.com">
-      <App />
-  </ GoogleOAuthProvider >,
+  React.createElement(
+    GoogleOAuthProvider,
+    { clientId: "20339060285-jbne00ieud7j9gv5t1u2bl3msucb82cr.apps.googleusercontent.com" },
+    React.createElement(App)
+  ),
   document.getElementById('root')
-)
+);
