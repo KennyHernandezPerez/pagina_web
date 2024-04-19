97de7b6f0afa5dbbd0b2d297b285f864e1d5ecff
KennyHernandezPerez <200300661@ucaribe.edu.mx>
2024-04-19 13:12:09 -0500

    archivos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
-import 'semantic-ui-css/semantic.min.css'
+import React from 'react';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Inicio from './paginas/Inicio';
-import Login from './paginas/Login';
-import Peliculas from './paginas/Peliculas';
-import Juegos from './paginas/Juegos';
-import Anime from './paginas/Anime';
-import Manga from './paginas/Manga';
-import Descripcion from './paginas/Descripcion';
-import Registro from './paginas/Registro';
-import Trivia from './paginas/Trivia';
-import Usuarios from './paginas/Dashboard/Dashboard_Usuarios';
-import Perfil from './paginas/Perfil';
-import DashListas from './paginas/Dashboard/Dashboard_Listas';
-import Resultados from './paginas/Busqueda';
-import ImageSearch from './paginas/ImagenBusqueda';
+import Inicio from './paginas/Inicio.js'; // Agregado .js
+import Login from './paginas/Login.js'; // Agregado .js
+import Peliculas from './paginas/Peliculas.js'; // Agregado .js
+import Juegos from './paginas/Juegos.js'; // Agregado .js
+import Anime from './paginas/Anime.js'; // Agregado .js
+import Manga from './paginas/Manga.js'; // Agregado .js
+import Descripcion from './paginas/Descripcion.js'; // Agregado .js
+import Registro from './paginas/Registro.js'; // Agregado .js
+import Trivia from './paginas/Trivia.js'; // Agregado .js
+import Usuarios from './paginas/Dashboard/Dashboard_Usuarios.js'; // Agregado .js
+import Perfil from './paginas/Perfil.js'; // Agregado .js
+import DashListas from './paginas/Dashboard/Dashboard_Listas.js'; // Agregado .js
+import Resultados from './paginas/Busqueda.js'; // Agregado .js
+import ImageSearch from './paginas/ImagenBusqueda.js'; // Agregado .js
 
 function App() {
   const router = createBrowserRouter([
-
     {
       path: "/",
       element: <Inicio />
@@ -75,15 +73,12 @@ function App() {
       path: "/imagen",
       element: <ImageSearch/>
     }
-
-  ])
+  ]);
 
   return (
-
     <div>
       <RouterProvider router={router} />
     </div>
-
   );
 }
 
--- frontend/src/main.mjs
+++ frontend/src/main.mjs
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import App from './App.jsx'; // No es necesario usar .default
+import App from './App.js'; // Cambiado a .js
 
 import './index.css'; // Importa directamente el archivo CSS
 
 const root = document.getElementById('root');
 
 ReactDOM.createRoot(root).render(
-  React.createElement(GoogleOAuthProvider, { clientId: "20339060285-jbne00ieud7j9gv5t1u2bl3msucb82cr.apps.googleusercontent.com" },
-    React.createElement(App, null)
-  )
+  <GoogleOAuthProvider clientId="20339060285-jbne00ieud7j9gv5t1u2bl3msucb82cr.apps.googleusercontent.com">
+    <App />
+  </GoogleOAuthProvider>
 );
