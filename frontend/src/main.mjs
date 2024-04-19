35996781202c7dbe47cf8e9e73c988553d79abc7
KennyHernandezPerez <200300661@ucaribe.edu.mx>
2024-04-19 12:57:57 -0500

    archivos

--- frontend/src/main.mjs
+++ frontend/src/main.mjs
@@ -5,8 +5,10 @@ import App from './App.jsx'; // No es necesario usar .default
 
 import './index.css'; // Importa directamente el archivo CSS
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId="20339060285-jbne00ieud7j9gv5t1u2bl3msucb82cr.apps.googleusercontent.com">
-    <App />
-  </GoogleOAuthProvider>
+const root = document.getElementById('root');
+
+ReactDOM.createRoot(root).render(
+  React.createElement(GoogleOAuthProvider, { clientId: "20339060285-jbne00ieud7j9gv5t1u2bl3msucb82cr.apps.googleusercontent.com" },
+    React.createElement(App, null)
+  )
 );
